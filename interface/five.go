60bc17a9b505516c225e805757a9aca54afe8b52
Vuong Nguyen <yeungon@gmail.com>
2025-02-10 11:03:58 +0700

    Interface- non struct

---
+++ interface/five.go
@@ -0,0 +1,30 @@
+package interface_go
+
+import "fmt"
+
+type animal interface {
+	breathe()
+	walk()
+}
+
+type cat string
+
+func (c cat) breathe() {
+	fmt.Println("Cat breathes")
+}
+
+func (c cat) walk() {
+	fmt.Println("Cat walk")
+}
+
+func Run5() {
+	var a animal
+
+	a = cat("smokey")
+	a.breathe()
+	a.walk()
+}
+
+// So far we have only seen examples of struct type implementing an interface.  It is also perfectly ok for any non-struct custom type to implement an interface
+
+//https://golangbyexample.com/interface-in-golang/
--- main.go
+++ main.go
@@ -9,4 +9,7 @@ import (
 func main() {
 	fmt.Println("Hello, world!")
 	interface_go.Run4()
+	interface_go.Run5()
 }
+
+//https://www.digitalocean.com/community/tutorials/how-to-use-interfaces-in-go
